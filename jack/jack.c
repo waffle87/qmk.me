acf6a966c4e7a176d0448f86fd32ee95f1594796
Jack Sangdahl <jack@pngu.org>
2025-08-24 09:57:53 -0600

    Add OS detection and modifier swapping for MacOS

--- jack/jack.c
+++ jack/jack.c
@@ -67,6 +67,28 @@ void matrix_scan_user(void) {
 #endif
 }
 
+#ifdef OS_DETECTION_ENABLE
+typedef struct {
+  bool swap_ctl_gui;
+  uint8_t unicode_input_mode;
+} os_detection_config;
+
+bool process_detected_host_os_user(os_variant_t detected_os) {
+  os_detection_config os_config = {.swap_ctl_gui = false,
+                                   .unicode_input_mode = UNICODE_MODE_LINUX};
+  if (detected_os == OS_MACOS)
+    os_config = (os_detection_config){.swap_ctl_gui = true,
+                                      .unicode_input_mode = UNICODE_MODE_MACOS};
+  keymap_config.swap_lctl_lgui = keymap_config.swap_rctl_rgui =
+      os_config.swap_ctl_gui;
+  eeconfig_update_keymap(&keymap_config);
+#ifdef UNICODE_COMMON_ENABLE
+  set_unicode_input_mode(os_config.unicode_input_mode);
+#endif
+  return true;
+}
+#endif
+
 #ifndef MAGIC_ENABLE
 uint16_t keycode_config(uint16_t keycode) { return keycode; }
 uint8_t mod_config(uint8_t mod) { return mod; }
