8baeb8b917d6b37a29f708e36640b9851f9185e2
Jack Sangdahl <jack@pngu.org>
2026-01-08 10:18:46 -0700

    Add vial keymap for sweep keyboard

---
+++ keyboards/sweep/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+// Copyright 2026 jack@pngu.org
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xF6, 0x5C, 0xA9, 0x90, 0x42, 0xBF, 0x02, 0xD4}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 4}
+#define VIAL_UNLOCK_COMBO_COLS {0, 4}
